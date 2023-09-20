68b31d70f142b5d6b5d148e6b857c76dea316f9b
ICE0208 <doky3307@naver.com>
2023-09-20 19:32:55 +0900

    home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-4xl mb-4">Best Seller Authors</h1>
+      {authors.map((item, index) => (
+        <li key={index}>
+          <Link className="hover:font-bold" to={`/${item.name}`}>
+            {item.name}
+          </Link>
+        </li>
+      ))}
+    </div>
+  );
 };
+
+interface IAuthor {
+  name: string;
+}
+
+const authors: IAuthor[] = [{ name: 'J.K.Rowling' }, { name: 'J.R.R.Tolkien' }];
 
 export default Home;
