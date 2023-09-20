7eef168d3d49b513172ca6d5f9024c373628c886
ICE0208 <doky3307@naver.com>
2023-09-21 01:02:04 +0900

    필요없는 코드 삭제

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
-import { NavLink, useMatch } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 interface ILink {
   to: string;
