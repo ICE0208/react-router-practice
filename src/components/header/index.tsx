79eb9759e9c84fe613fced16e2cddc2d63a82d08
ICE0208 <doky3307@naver.com>
2023-09-20 19:26:18 +0900

    text hover됐을 때만 색 변화

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -14,8 +14,10 @@ const Header = () => (
   <div className="px-2 py-4">
     <ul className="list-disc list-inside text-xl">
       {links.map((item, index) => (
-        <li key={index} className="hover:text-blue-400">
-          <Link to={item.to}>{item.label}</Link>
+        <li key={index}>
+          <Link className="hover:text-blue-400" to={item.to}>
+            {item.label}
+          </Link>
         </li>
       ))}
     </ul>
