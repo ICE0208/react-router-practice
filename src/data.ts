c8891d086ac795ad73e4d72abd79048879f1893f
ICE0208 <doky3307@naver.com>
2023-09-21 00:57:39 +0900

    데이터 수정

--- src/data.ts
+++ src/data.ts
@@ -4,13 +4,23 @@ export const authorDatas: IAuthorData[] = [
     books: [
       {
         bookTitle: 'Harry Potter I',
-        chapters: ['Chapter I', 'Chapter II', 'Chapter III', 'Chapter IV'],
-        characters: ['Good Guy A', 'Good Guy B', 'Bad Guy A', 'Bad Guy B'],
+        chapters: [
+          'Chapter I - aab',
+          'Chapter II - aba',
+          'Chapter III - bab',
+          'Chapter IV - final',
+        ],
+        characters: ['Good Guy A', 'Good Guy A2', 'Bad Guy A3', 'Bad Guy A4'],
       },
       {
         bookTitle: 'Harry Potter II',
-        chapters: ['Chapter I', 'Chapter II', 'Chapter III', 'Chapter IV'],
-        characters: ['Good Guy A', 'Good Guy B', 'Bad Guy A', 'Bad Guy B'],
+        chapters: [
+          'Chapter I - 111',
+          'Chapter II - 222',
+          'Chapter III - 333',
+          'Chapter IV - 444',
+        ],
+        characters: ['Good Guy B', 'Good Guy B2', 'Bad Guy B3', 'Bad Guy B4'],
       },
     ],
   },
@@ -20,13 +30,23 @@ export const authorDatas: IAuthorData[] = [
     books: [
       {
         bookTitle: 'Lord of The Rings I',
-        chapters: ['Chapter I', 'Chapter II', 'Chapter III', 'Chapter IV'],
-        characters: ['Good Guy A', 'Good Guy B', 'Bad Guy A', 'Bad Guy B'],
+        chapters: [
+          'Chapter I - a',
+          'Chapter II - b',
+          'Chapter III - c',
+          'Chapter IV - d',
+        ],
+        characters: ['Good Guy C', 'Good Guy C2', 'Bad Guy C3', 'Bad Guy C4'],
       },
       {
         bookTitle: 'Lord of The Rings II',
-        chapters: ['Chapter I', 'Chapter II', 'Chapter III', 'Chapter IV'],
-        characters: ['Good Guy A', 'Good Guy B', 'Bad Guy A', 'Bad Guy B'],
+        chapters: [
+          'Chapter I - aa',
+          'Chapter II - bb',
+          'Chapter III - cc',
+          'Chapter IV - dd',
+        ],
+        characters: ['Good Guy D', 'Good Guy D2', 'Bad Guy D3', 'Bad Guy D4'],
       },
     ],
   },
